bc2583dfdc5c8e23f5ae194b8e88aed01790c1f5
Thanapat Pirmphol <thanapat.pirmphol@unilode.com>
2018-08-06 18:24:02 +0700

    no message

--- extractor.py
+++ extractor.py
@@ -6,6 +6,7 @@ base_path = './masterdata'
 mongo = MongoClient()
 db = mongo.planb_prod_main
 
+'''
 with open(os.path.join(base_path, 'carriers'), 'w') as f:
     for carrier in db.carriers.find():
         if str(carrier['carrierCode']).strip() != '':
@@ -40,4 +41,20 @@ with open(os.path.join(base_path, 'ulds'), 'w') as f:
     for uld in db.ulds.find():
         if str(uld['uldCode']).strip() != '':
             f.write(uld['uldCode'] + '\n')
+    f.close()
+'''
+
+with open(os.path.join(base_path, 'flights'), 'w') as f:
+    for fullFlightNbr in db.flights.find({}).distinct('fullFlightNbr'):
+        if len(fullFlightNbr) > 3:
+            f.write(fullFlightNbr + '\n')
+    f.close()
+
+    
+
+db = mongo.planb_prod_stats
+with open(os.path.join(base_path, 'flights_changes'), 'w') as f:
+    f.write('error,correction\n')
+    for log in db.debug_msgQuality.find({ 'chg_flightNbr': { '$ne': None } }):
+        f.write(log['carrierCode'] + log['flightNbr'] + ',' + log['carrierCode'] + log['chg_flightNbr']  + '\n')
     f.close()
